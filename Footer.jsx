b2231cbb92c6ad34b2c6d92198ab718f3f3c3332
AKartheek <86522128+AKartheek@users.noreply.github.com>
2021-06-26 18:40:57 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+    <p>
+      copyright by Kartheek @ {new Date().getFullYear()}
+    </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+     <h1>My Dad,mom,sister,grand mother and father </h1>
+     <p>This is my family and i love my family</p>
+  
+    </div>
+  );
+}
+
+export default Info;
